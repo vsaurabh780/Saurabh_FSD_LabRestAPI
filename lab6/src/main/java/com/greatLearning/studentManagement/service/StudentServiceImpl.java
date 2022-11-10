7772ce24f5771b57680f22fefee8d328882d3bc4
Saurabh verma <107805245+vsaurabh780@users.noreply.github.com>
2022-11-10 22:58:38 +0530

    Add files via upload

---
+++ lab6/src/main/java/com/greatLearning/studentManagement/service/StudentServiceImpl.java
@@ -0,0 +1,51 @@
+package com.greatLearning.studentManagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.greatLearning.studentManagement.entity.Student;
+import com.greatLearning.studentManagement.repository.StudentRepository;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+@Repository
+public class StudentServiceImpl implements StudentService {
+
+	@Autowired
+	StudentRepository studentRepository;
+
+	@Transactional
+	public List<Student> findAll() {
+
+		List<Student> students = studentRepository.findAll();
+
+		return students;
+	}
+
+	@Transactional
+	public Student findById(int id) {
+
+		Student student = new Student();
+		student = studentRepository.findById(id).get();
+
+		return student;
+	}
+
+	@Transactional
+	public void save(Student theStudent) {
+
+		studentRepository.save(theStudent);
+
+	}
+
+	@Transactional
+	public void deleteById(int id) {
+
+		studentRepository.deleteById(id);
+
+	}
+
+}
+
